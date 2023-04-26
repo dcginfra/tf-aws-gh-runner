23f8e49c9250e841ea9d2aaa7ca72c275c99f803
strophy <badfunkstripe@gmail.com>
2023-04-26 16:29:36 +1000

    chore: move cache resources to separate file

--- examples/platform/main.tf
+++ examples/platform/main.tf
@@ -38,8 +38,8 @@ module "runners" {
 
   runner_extra_labels = "default,example"
 
-  enable_ephemeral_runners = true
-  runners_maximum_count = 15
+  enable_ephemeral_runners          = true
+  runners_maximum_count             = 15
   runners_scale_down_lambda_timeout = 10
 
   block_device_mappings = [{
@@ -79,81 +79,4 @@ module "runners" {
 
   # override scaling down
   scale_down_schedule_expression = "cron(* * * * ? *)"
-}
-
-data "aws_ami" "docker_cache_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-data "aws_security_group" "runner_sg" {
-  tags = {
-    "ghr:environment" = local.environment
-  }
-}
-
-resource "aws_security_group" "ssh_access_cache" {
-  name_prefix = "${local.environment}-ssh-access-cache-sg"
-
-  vpc_id = module.base.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ssh-access-cache"
-  }
-}
-
-resource "aws_instance" "docker_cache" {
-  ami           = data.aws_ami.docker_cache_ami.id
-  instance_type = "t4g.micro"
-
-  subnet_id = module.base.vpc.public_subnets[0]
-  vpc_security_group_ids = [
-    data.aws_security_group.runner_sg.id,
-    aws_security_group.ssh_access_cache.id
-    ]
-
-  user_data_replace_on_change = true
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-              echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-              apt-get update -y
-              apt-get install -y docker-ce docker-ce-cli containerd.io
-              usermod -aG ubuntu docker
-              echo -e "---\n\nversion: 0.1\nlog:\n  level: info\n  fields:\n    service: registry\nstorage:\n  cache:\n    blobdescriptor: inmemory\n  filesystem:\n    rootdirectory: /var/lib/registry\nhttp:\n  addr: :5000\n  headers:\n    X-Content-Type-Options: [nosniff]\nproxy:\n  remoteurl: https://registry-1.docker.io" > /home/ubuntu/config.yml
-              mkdir /home/ubuntu/registry
-              docker run -d -p 443:5000 --restart=always --name=through-cache -v /home/ubuntu/config.yml:/etc/docker/registry/config.yml -v /home/ubuntu/registry:/var/lib/registry registry:2
-              EOF
-
-  associate_public_ip_address = true
-  key_name = "dashdev.rsa"
-
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 20
-  }
-
-  tags = {
-    Name = "platform-docker-cache-tf"
-  }
 }
