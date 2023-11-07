16d3a1e8145280b6caf942420104d8b78011a739
strophy <badfunkstripe@gmail.com>
2023-11-07 14:39:14 +1000

    feat: create oidc connect provider for github

--- examples/multi-runner/main.tf
+++ examples/multi-runner/main.tf
@@ -98,3 +98,9 @@ module "webhook_github_app" {
   }
   webhook_endpoint = module.runners.webhook.endpoint
 }
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+  tags            = local.tags
+}
